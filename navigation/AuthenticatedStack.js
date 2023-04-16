e8944791535b43477f87771bf3fb6941a2043aef
NareshPatilOfficial <nareshpatil.nrp@gmail.com>
2023-04-16 12:58:30 +0530

    Added new iconButton component, and used it as logout button and added logout logic

--- navigation/AuthenticatedStack.js
+++ navigation/AuthenticatedStack.js
@@ -1,16 +1,22 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import WelcomeScreen from "../screens/WelcomeScreen";
 import { Colors } from "../constants/styles";
+import IconButton from "../component/UI/IconButton";
+import { useContext } from "react";
+import { AuthContext } from "../store/auth-context";
 
 const Stack = createNativeStackNavigator();
 
 function AuthenticatedStack(){
+    const authCnx = useContext(AuthContext);
+
     return (
         <Stack.Navigator
             screenOptions={{
                 headerStyle: { backgroundColor: Colors.primary500 },
                 headerTintColor: 'white',
-                contentStyle: { backgroundColor: Colors.primary100 }
+                contentStyle: { backgroundColor: Colors.primary100 },
+                headerRight: (({tintColor}) => <IconButton name={'exit'} color={tintColor} size={20} onPress={authCnx.logOut} />)
             }}
         >
             <Stack.Screen name='Welcome' component={WelcomeScreen} />
